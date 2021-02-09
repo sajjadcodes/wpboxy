965fb8f47947b0cc206af7e82bd11d900cd08d87
sajadhussain <sakraan01@gmail.com>
2021-02-09 17:11:54 +0500

    footer v-1

--- footer.php
+++ footer.php
@@ -22,7 +22,7 @@
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'wpboxy' ), 'wpboxy', '<a href="https://sajjadcodes.com/">Sajjad Hussain</a>' );
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'wpboxy' ), 'wpboxy', '<a href="https://sajjadcodes.com/" class="main__footer--copyright">Sajjad Hussain</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
@@ -34,10 +34,3 @@
 </html>
 
 
-<footer class="main__footer">
-            <div class="container">
-                <p class="main__footer--copyright">©2020, Theme. All Rights Reserved.</p>
-            </div>
-        </footer>
-</body>
-</html>
